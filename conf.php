1cb0eef9f7da2a3569a9767a716875324680dedc
waming <honghua1207@sina.com>
2015-08-18 15:03:41 +0800

    conf.php
    配置文件

---
+++ conf.php
@@ -0,0 +1,8 @@
+'REDIS_CONF'            => array(
+                                   'master' => array(
+                                      array('host'=> '127.0.0.1', 'auth' => '', 'port' => 6379),
+                                   ),  
+                                   'slave'  => array(
+                                      array('host'=> '127.0.0.1', 'auth' => '', 'port' => 6379),
+                                   ),
+                            ), 
